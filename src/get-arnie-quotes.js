3f85123243c4d8cd76c263ef9c52a25b08cab72b
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2022-01-20 19:38:24 +1100

    smokeball javascript test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,39 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = async urls => {
+	return await Promise.all(
+		urls.map(async url => {
+			const response = await httpGet(url);
+			if (response.status === 200) {
+				return {
+					"Arnie Quote": JSON.parse(response.body).message,
+				};
+			} else {
+				return {
+					FAILURE: JSON.parse(response.body).message,
+				};
+			}
+		})
+	);
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
+
+// Previous solution
+// for (index = 0; index < urls.length; index++) {
+//   const response = await httpGet(urls[index]);
+//   if (response.status === 200) {
+//     results.push({
+//       "Arnie Quote": JSON.parse(response.body).message,
+//     });
+//   } else {
+//     results.push({
+//       FAILURE: JSON.parse(response.body).message,
+//     });
+//   }
+// }
+// return new Promise(function (resolve, reject) {
+//   resolve(results);
+// });
